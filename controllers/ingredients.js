f0290b39cc34b71a103ba1ad231cf3db065b23d9
kiran1926 <kiranroge19@gmail.com>
2025-03-14 14:10:29 -0500

    wip: required imports

---
+++ controllers/ingredients.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+const User = require("../models/user.js");
+const Recipe = require("../models/recipe.js");
+const Ingredient = require("../models/ingredient.js");
+
+
+
+
+module.exports = router;
